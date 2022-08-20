1c285bac3b566502345b99bc76716cfdc05bb58d
Ana Pisano <ani_pisano@hotmail.com>
2022-08-20 13:15:05 -0300

    primera entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let imagen = "kqXzqebG"
+  let titulo = "Hamburguesa con queso"
+  let calorias = "243"
+  let descripcion = "El sabor de la carne 100% vacuna más deliciosa, acompañado del pan más esponjoso, kétchup, mostaza y cebolla triturada."
+
+  const producto = {id:1, imagen:imagen, title:titulo, calories:calorias, description:descripcion }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-4 offset-md-2'>
+          <img src={imagen} alt={producto.title} className="img-fluid"/>
+        </div>
+        <div className='col-md-4'>
+          <h1>{producto.title}</h1>
+          <p>{producto.calories}</p>
+          <p>{producto.description}</p>
+
+        </div>
+      </div>
     </div>
   );
 }
